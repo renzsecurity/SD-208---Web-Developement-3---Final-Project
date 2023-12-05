6140d5c4fc975ba87fd73cf4e62b1ddf914407ac
renzsecurity <22104728@usc.edu.ph>
2023-12-05 21:13:26 +0800

    second commit

---
+++ SD 208 - Web Developement 3 - Final Project/Controllers/activitiesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SD_208___Web_Developement_3___Final_Project.Models;
+
+namespace SD_208___Web_Developement_3___Final_Project.Controllers
+{
+    public class activitiesController : Controller
+    {
+        private UserDBContext db = new UserDBContext();
+
+        // GET: activities
+        public ActionResult Index()
+        {
+            var activities = db.activities.Include(a => a.user);
+            return View(activities.ToList());
+        }
+
+        // GET: activities/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            activity activity = db.activities.Find(id);
+            if (activity == null)
+            {
+                return HttpNotFound();
+            }
+            return View(activity);
+        }
+
+        // GET: activities/Create
+        public ActionResult Create()
+        {
+            ViewBag.user_id = new SelectList(db.users, "user_id", "firstname");
+            return View();
+        }
+
+        // POST: activities/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "activity_id,user_id,activity_title,activity_description,activity_date,created_at")] activity activity)
+        {
+            if (ModelState.IsValid)
+            {
+                db.activities.Add(activity);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.user_id = new SelectList(db.users, "user_id", "firstname", activity.user_id);
+            return View(activity);
+        }
+
+        // GET: activities/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            activity activity = db.activities.Find(id);
+            if (activity == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.user_id = new SelectList(db.users, "user_id", "firstname", activity.user_id);
+            return View(activity);
+        }
+
+        // POST: activities/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "activity_id,user_id,activity_title,activity_description,activity_date,created_at")] activity activity)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(activity).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.user_id = new SelectList(db.users, "user_id", "firstname", activity.user_id);
+            return View(activity);
+        }
+
+        // GET: activities/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            activity activity = db.activities.Find(id);
+            if (activity == null)
+            {
+                return HttpNotFound();
+            }
+            return View(activity);
+        }
+
+        // POST: activities/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            activity activity = db.activities.Find(id);
+            db.activities.Remove(activity);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
